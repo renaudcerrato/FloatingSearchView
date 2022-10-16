5cc766cfd6c21ffde75276429f24b0bdc3e5489e
IODevBlue <iodevblue@gmail.com>
2022-10-16 16:12:25 +0100

    Fixed Retrofit dependencies in RetrofitModule

--- sample/src/main/java/com/mypopsy/floatingsearchview/demo/dagger/RetrofitModule.java
+++ sample/src/main/java/com/mypopsy/floatingsearchview/demo/dagger/RetrofitModule.java
@@ -1,16 +1,15 @@
 package com.mypopsy.floatingsearchview.demo.dagger;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.logging.HttpLoggingInterceptor;
-
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import retrofit.Converter;
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class RetrofitModule {
